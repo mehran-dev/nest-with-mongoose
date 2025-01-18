2a189db5832185654f4aa35c92c6b3a54c8077d4
mehran.saghebifard <m.saghebi.dev@gmail.com>
2025-01-18 11:04:30 +0330

    wip

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AppointmentService {
-  create(createAppointmentDto: CreateAppointmentDto) {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createAppointmentDto: CreateAppointmentDto) {
+    const appointments = await this.prismaService.appointment.create({
+      data: {
+        created_at: createAppointmentDto.created_at,
+        date: createAppointmentDto.date,
+        end_time: createAppointmentDto.end_time,
+        start_time: createAppointmentDto.start_time,
+        updated_at: createAppointmentDto.updated_at,
+        person_id: createAppointmentDto.person_id,
+        reserved_by: createAppointmentDto.reserved_by,
+        room: createAppointmentDto.room,
+      },
+    });
+
     return 'This action adds a new appointment';
   }
 
-  findAll() {
-    return `This action returns all appointment`;
+  async findAll() {
+    const appointments = await this.prismaService.appointment.findMany({});
+
+    if (!appointments.length) {
+      throw new NotFoundException();
+    }
+    return appointments;
   }
 
   findOne(id: number) {
